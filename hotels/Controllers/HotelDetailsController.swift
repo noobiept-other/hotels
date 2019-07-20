810c3093cb2b64222f8a1dbccef8d14d52e71e9b
Pedro Ferreira <plferreira4@gmail.com>
2019-07-20 16:01:42 +0100

    Update the rest of the hotel information on the 'hotel details' view.

--- hotels/Controllers/HotelDetailsController.swift
+++ hotels/Controllers/HotelDetailsController.swift
@@ -3,7 +3,21 @@ import UIKit
 class HotelDetailsController: UITableViewController {
 
     var hotel: Hotel?
+
     @IBOutlet var name: UILabel!
+    @IBOutlet var stars: UILabel!
+    @IBOutlet var userRating: UILabel!
+    @IBOutlet var address: UILabel!
+    @IBOutlet var city: UILabel!
+    @IBOutlet var latitude: UILabel!
+    @IBOutlet var longitude: UILabel!
+    @IBOutlet var checkInFrom: UILabel!
+    @IBOutlet var checkInTo: UILabel!
+    @IBOutlet var checkOutFrom: UILabel!
+    @IBOutlet var checkOutTo: UILabel!
+    @IBOutlet var phoneNumber: UILabel!
+    @IBOutlet var email: UILabel!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,5 +27,17 @@ class HotelDetailsController: UITableViewController {
         }
 
         self.name.text = hotel.name
+        self.stars.text = String(hotel.stars)
+        self.userRating.text = String(hotel.userRating)
+        self.address.text = hotel.location.address
+        self.city.text = hotel.location.city
+        self.latitude.text = String(hotel.location.latitude)
+        self.longitude.text = String(hotel.location.longitude)
+        self.checkInTo.text = hotel.checkIn.to
+        self.checkInFrom.text = hotel.checkIn.from
+        self.checkOutTo.text = hotel.checkOut.to
+        self.checkOutFrom.text = hotel.checkOut.from
+        self.phoneNumber.text = hotel.contact.phoneNumber
+        self.email.text = hotel.contact.email
     }
 }
